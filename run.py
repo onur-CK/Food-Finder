7f08c18b43c94fcbe5910ddae5b030b89ea78916
Onur Capik <cpkonr@gmail.com>
2024-04-24 02:44:05 +0000

    Delete food advice function and take the suggestions list out for making the suggestions list globally reachable.

--- run.py
+++ run.py
@@ -205,10 +205,6 @@ suggestions = {
             
         
      
-    
-
-
-
 cuisine_preference = get_cuisine_choice()
 print(f"You selected {cuisine_preference} cuisine.\n")
 
@@ -232,63 +228,3 @@ if food_preferences["food_type"] in ("Vegetarian", "Non-Vegetarian"):
 
 
 
-def food_advice(cuisine, food_type):
-    """Suggests food based on the user's preferences."""
-    suggestions = {
-        "Chinese": {
-            "Vegetarian": {
-                "Rice": {
-                    "Fried Rice": ["Yangzhou fried rice", "Vegetable lo mein, Spicy tofu fried rice"],
-                    "Soup and Rice Noodles": ["Buddha's delight", "Mapo tofu rice bowl", "Spicy eggplant with rice"],
-                "Noodle": {
-                    "Spicy": ["Sichuan dan dan noodles", "Spicy tofu noodle soup", "Spicy zha jiang mian"],
-                    "Classic Savory": ["Sesame noodle salad(Liang mian)", "Vegetable lo mein", "Wombock noodle soup"]
-                },
-                },
-            "Non-Vegetarian": {
-                "Sea Food": {
-                    "Classic Seafood": ["Steamed whole fish", "Stir-fried shrimp with snow peas", "Braised fish with black bean sauce"],
-                    "Spicy Seafood": ["Kung pao prwans", "Spicy mussel hot pot", "Spicy garlic lobster"],
-                "Red Meat": {
-                    "Stir Fries": ["Kung pao beef", "Black pepper beef", "Beef and broccoli"],
-                    "Braised Foods": ["Braised  pork belly with brown sauce", "Spich sichuan beef stew", "Lion's head meatballs"],
-                "Chicken": {
-                    "Stir Fries Chicken": ["Cashew chicken", "Chicken with garlic sauce", "Hunan chicken"],
-                    "Noodle Soups": ["Wonton soup with chicken", "Spicy chicken noodle soup", "Chicken noodle soup with egg drop"]
-                }
-                } 
-                },
-        "Turkish": {
-            "Vegetarian": {
-                "Main Courses": {
-                    "Stuffed Vegetables": ["Imam bayildi", "Kabak dolmasi(stuffed zucchini)", "Biber dolmasi(Stuffed peppers)"],
-                    "Savory Fritters and Pancakes": ["Mucver", "Gozleme", "Cigar boreks"],
-                "Lentil and Chickpea": {
-                    "Stew and soups": ["Lentil soup", "Chickpea stew", "Zucchini fritter soup"],
-                    "Salads and colds": ["Chichkpea salad", "Lentil salad", "White bean salad"],
-            "Non-Vegetarian": {
-                "Sea Food": {
-                    "Fresh fish dishes": ["Levrek izgara(seabass)", "Hamsili Pilav", "Alinazik with shrimp"],
-                    "Seafood stews and soups": ["Fish stew", "seafood soup", "Dalyan dolmasi(stuffed mussels)"],
-                "Red Meat": {
-                    "Kebabs": ["Doner kebab", "Adana kebab", "Shish kebab"],
-                    "Stews and Braises": ["Hunkar begendi", "Osmanli kebab"],
-                "Chicken": {
-                    "Grilled": ["Tavuk shish", "Alinazik", "Iskender chicken"],
-                    "Baked and Casserole": ["Chicken guvec", "Sultan's delight", "Chicken and pita bread casserole(pide)"]
-                }
-                }
-                }
-            }
-                }
-                }
-            }
-        }
-            } 
-            
-        }
-     }
-    } 
-    #print(f"{suggestions}")
-
-#food_advice("Chinese, Turkish", "Vegetarian, Non-Vegetarian")
